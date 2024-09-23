d893f09cbae401bc156f6d43ed8af5d7389fd31e
IT24102769 <it24102769@my.sliit.lk>
2024-09-23 14:22:05 +0530

    IT24102769Lab7Q2c

---
+++ Q2/IT24102769Lab7Q2c.java
@@ -0,0 +1,19 @@
+public class IT24102769Lab7Q2c
+{
+ public static void main(String[]args)
+ {
+  int i,j;
+  
+  for(i=5; i>=1; i--)
+  {
+   
+   for(j=1; j<=i; j++)
+   {
+   System.out.print(i);
+   } 
+
+  System.out.print("\n");
+  }
+   
+ }
+}
