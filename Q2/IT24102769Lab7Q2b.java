0187572199aebebb9f30ded9656bc7afdef801b9
IT24102769 <it24102769@my.sliit.lk>
2024-09-23 14:19:57 +0530

    IT24102769Lab7Q2b

---
+++ Q2/IT24102769Lab7Q2b.java
@@ -0,0 +1,19 @@
+public class IT24102769Lab7Q2b
+{
+ public static void main(String[]args)
+ {
+  int i,j;
+  
+  for(i=1; i<=5; i++)
+  {
+   System.out.print(i + " - ");
+   for(j=1; j<=i; j++)
+   {
+   System.out.print("* ");
+   } 
+
+  System.out.print("\n");
+  }
+   
+ }
+}
